9cb0475fceeec5f73dfb4c2cde2563826de10d04
Faye Rosenshein <rosefaye.a@gmail.com>
2023-04-11 12:42:14 -0600

    Add react router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
+import { getTopStoryData } from '../../ApiCalls';
 import './App.css';
 
 function App() {
+	const [topStories, setTopStories] = useState([])
+
+	useEffect(() => {
+		getTopStoryData('home')
+		.then((data) => {
+			if(!data.ok) {
+				throw new Error('Failed to fetch top stories')
+			}
+			return data.json()
+		})
+		.then((data) => {
+			setTopStories(data.results)
+			console.log(topStories)
+		})
+	},[])
+
   return (
     <main>
 			<h1>Take Home Challenge</h1>
+			<link></link>
 		</main>
   );
 }
